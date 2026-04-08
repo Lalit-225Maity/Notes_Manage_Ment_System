0c1dc0bdf75f192a000b08466d3a371cbc87d99e
LALIT MAITY <maitylalit89@gmail.com>
2026-04-09 01:23:52 +0530

    I correct

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,26 +2,25 @@ import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
-import './Dashboard.css'
+ 
 import { useLocation } from 'react-router-dom'
-import {Helmet} from 'react-helmet'
-const Dashboard = ({setcr}) => {
+import { Helmet } from 'react-helmet'
+const Dashboard = ({ setcr }) => {
     const location = useLocation();
 
     return (
         <div className='dashboard'>
-        <Helmet>
-            <title>LalitNest</title>
-          <link rel="shortcut icon" href="/notes.png" type="image/x-icon" />
-        </Helmet>
+            <Helmet>
+                <title>LalitNest</title>
+                <link rel="shortcut icon" href="/notes.png" type="image/x-icon" />
+            </Helmet>
             <Sidebar />
-            <div className="nav-out">
-                {!location.pathname.includes('profile') && <Navbar setcr={setcr}/>}
-                <div className="content">
-                    <Outlet />
-                </div>
-            </div>
+
+            {!location.pathname.includes('profile') && <Navbar setcr={setcr} />}
+            <Outlet />
+
         </div>
+         
     )
 }
 
