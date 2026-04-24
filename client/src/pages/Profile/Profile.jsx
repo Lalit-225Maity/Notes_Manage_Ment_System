7de23ecf9de49513d94ca8d7c1f73b7856e2f913
LALIT MAITY <maitylalit89@gmail.com>
2026-04-24 14:59:35 +0530

    All added

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ComposedChart, Cell } from 'recharts';
- 
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ComposedChart, Cell, Scatter, Area, Line } from 'recharts';
+
 import axios from 'axios'
 import './Profile.css'
 const Profile = () => {
@@ -9,6 +9,7 @@ const Profile = () => {
   const [imp, setimp] = useState([])
   const [personals, setpersonals] = useState([])
   const [userProfile, setuserProfile] = useState(null)
+
   useEffect(() => {
     const Item = localStorage.getItem("Profile");
     if (Item) {
@@ -16,25 +17,25 @@ const Profile = () => {
 
     }
   }, [])
-   useEffect(() => {
+  useEffect(() => {
     (async () => {
       try {
         const response = await axios.get('/api/fetchnote');
-        const checkvalue=response.data.filter((i)=>{
-          return i.tag==='Pinned'
+        const checkvalue = response.data.filter((i) => {
+          return i.tag === 'Pinned'
         })
         setval(checkvalue);
-         const personal_val=response.data.filter((i)=>{
-          return i.tag==='Personal'
-         })
-         setpersonals(personal_val)
-         const Important_values=response.data.filter((i)=>{
-          return i.tag==='Important'
-         })
-          
-         setimp(Important_values)
-         setworkdetail(response.data.filter(i=>i.tag==='Work'))
-        
+        const personal_val = response.data.filter((i) => {
+          return i.tag === 'Personal'
+        })
+        setpersonals(personal_val)
+        const Important_values = response.data.filter((i) => {
+          return i.tag === 'Important'
+        })
+
+        setimp(Important_values)
+        setworkdetail(response.data.filter(i => i.tag === 'Work'))
+
 
       } catch (error) {
         console.log(error);
@@ -42,14 +43,14 @@ const Profile = () => {
       }
     })();
   }, [])
- 
-  const data = [
-    { name:"Pinned",  note:val.length,color:"#E87F24"},
-    {name:"Personal", note:personals.length,color: "#5E7AC4" },
-    {name:"Important" , note:imp.length,color:"#FF5A5A" },
-    {name:"Work",note:workdetail.length,color:"#85193C" }
 
- 
+  const data = [
+    { name: "Pinned", note: val.length, color: "#E87F24" },
+    { name: "Personal", note: personals.length, color: "#5E7AC4" },
+    { name: "Important", note: imp.length, color: "#FF5A5A" },
+    { name: "Work", note: workdetail.length, color: "#85193C" }
+
+
   ];
 
   return (
@@ -57,37 +58,72 @@ const Profile = () => {
       <div className="user-details">
         {userProfile &&
           <div className="profile-detail">
-            <h4>{userProfile.name}</h4>
-            <p>{userProfile.emailID}</p>
-            <p>{userProfile.phoneNumber}</p>
+            <div className="profile-user" >
+              <button>{userProfile.name.charAt(0)}</button>
+              <div className="user-other-details">
+                <h4>{userProfile.name}</h4>
+                <p>Email ID : {userProfile.emailID}</p>
+                <p>Phone Number : {userProfile.phoneNumber}</p>
+              </div>
+            </div>
+            <BarChart
+              style={{ width: '60%', maxHeight: '60vh', aspectRatio: 1.618 }}
+              responsive
+              data={data}
+
+              margin={{
+                top: 5,
+                right: 0,
+                left: 0,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" vertical={false} />
+              <XAxis dataKey="name" />
+              <YAxis width={50} domain={[0, 100]} ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+              />
+              <Tooltip />
+
+              <Bar dataKey="note" activeBar={false} radius={[10, 10, 0, 0]} maxBarSize={60} >
+                {data.map((i, key) => (
+                  <Cell key={key} fill={i.color} />
+                ))}
+              </Bar>
+
+
+            </BarChart>
+            <ComposedChart
+              style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+              responsive
+              data={data}
+              margin={{
+                top: 20,
+                right: 0,
+                bottom: 0,
+                left: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" vertical={false} />
+              <XAxis dataKey="name" scale="band" />
+              <YAxis width="auto" niceTicks="snap125" domain={[0, 10]} ticks={[1,2,3,4,5,6,7,8,9,10]}/>
+              <Tooltip />
+
+              <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+              <Bar dataKey="note" maxBarSize={60}   >
+                {data.map((i, key) => (
+                  <Cell key={key} fill={i.color} />
+                ))}
+              </Bar>
+              <Line type="monotone" dataKey="note" stroke="#ff7300" />
+
+
+            </ComposedChart>
+
+
           </div>
         }
       </div>
-      <BarChart
-        style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-        responsive
-        data={data}
-    
-        margin={{
-          top: 5,
-          right: 0,
-          left: 0,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="name" />
-        <YAxis  width={50}  />
-        <Tooltip />
-        
-        <Bar dataKey="note"  activeBar={false} radius={[10, 10, 0, 0]}  maxBarSize={60} >
-             {data.map((i,key)=>(
-              <Cell key={key} fill={i.color}/>
-             ))}
-        </Bar>
-        
 
-      </BarChart>
 
     </div>
   )
